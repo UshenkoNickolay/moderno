2577109d7fb52c97cff23a9d5f115053ec874580
UshenkoNickolay <arnofayt@gmail.com>
2021-08-10 20:25:55 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
 
+  // $('.rate-star').rateYo({
+  //   rating: 3.6,
+  //   starWith: '12px',
+  // });
 
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToScroll: 4,
+    slidesToShow: 4,
+  });
 
+var mixer = mixitup('.products__inner-box');
 
 
 });
