26624eed3f09e29a81274c73622743d3151138e5
UshenkoNickolay <arnofayt@gmail.com>
2021-08-17 19:30:08 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,41 @@ $(function(){
   //   starWith: '12px',
   // });
 
-  $('.product-slider__inner').slick({
-    dots: true,
-    arrows: false,
-    slidesToScroll: 4,
-    slidesToShow: 4,
-  });
+$('.product-slider__inner').slick({
+  dots: true,
+  arrows: false,
+  slidesToScroll: 4,
+  slidesToShow: 4,
+});
+
+// $('.js-range-slider').ionRangeSlider({
+//   type: 'double',
+//   min: 0,
+//   max: 1000,
+//   from: 0,
+//   to: 600,
+//   prefix: '$'
+// });
+
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
+});
+$('.icon-th-large').on('click', function(){
+  $('.product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+});
+
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
 
 var mixer = mixitup('.products__inner-box');
 
