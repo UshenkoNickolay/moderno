75e85259df3e5a9d50189c13feca3a9a68fb280a
UshenkoNickolay <arnofayt@gmail.com>
2021-08-18 18:40:34 +0300

    added page 404 and page about us

--- app/js/main.js
+++ app/js/main.js
@@ -34,7 +34,7 @@ $('.icon-th-large').on('click', function(){
 
 $('.product-one__tabs .tab').on('click', function(event) {
   var id = $(this).attr('data-id');
-    $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
     $('.product-one__tabs .tabs').find('.tab').removeClass('active');
     $(this).addClass('active');
     $('#'+id).addClass('active-tab').fadeIn();
