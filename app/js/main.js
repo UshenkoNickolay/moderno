6c3bb39a9bc96db3ae67c5e112b9463ea3425b58
UshenkoNickolay <arnofayt@gmail.com>
2021-08-22 14:44:07 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,31 @@ $('.product-slider__inner').slick({
   arrows: false,
   slidesToScroll: 4,
   slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1441,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 801,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+  ]
 });
 
 // $('.js-range-slider').ionRangeSlider({
